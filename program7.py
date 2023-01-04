08737276b95dbb0562987bd68f723fd6b5830e39
Anugya Maurya <49150938+AnugyaMaurya@users.noreply.github.com>
2023-01-05 00:20:49 +0530

    Create program7.py

---
+++ program7.py
@@ -0,0 +1,10 @@
+'''Return sum of values in a matrix'''
+
+import numpy as np
+             
+gfg = np.matrix('[4, 1; 12, 3]')
+             
+# applying matrix.sum() method
+geek = gfg.sum()
+   
+print(geek)
