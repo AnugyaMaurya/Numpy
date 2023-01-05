415838694ed4fbf18829b839bb69b8f66fa0b88a
Anugya Maurya <49150938+AnugyaMaurya@users.noreply.github.com>
2023-01-05 13:08:31 +0530

    Create program9.py

---
+++ program9.py
@@ -0,0 +1,18 @@
+'''count the frequency of unique values in NumPy array'''
+
+import numpy as np
+  
+ini_array = np.array([10, 20, 5,
+                      10, 8, 20,
+                      8, 9])
+  
+
+unique, frequency = np.unique(ini_array, 
+                              return_counts = True)
+
+print("Unique Values:", 
+      unique)
+  
+
+print("Frequency Values:",
+      frequency)
