3bb934d9b6c106ca994ad7befc4e8132073fece2
Anugya Maurya <49150938+AnugyaMaurya@users.noreply.github.com>
2023-01-05 13:02:46 +0530

    Create program8.py

---
+++ program8.py
@@ -0,0 +1,11 @@
+'''matrix multiplication ,addition,division,subtraction'''
+
+import numpy as np
+  
+
+p = np.matrix([[1, 2], [2, 3]])
+q = np.matrix([[4, 5], [6, 7]])
+mul=p*q
+sub=p-q
+add=p+q
+div=p/q
